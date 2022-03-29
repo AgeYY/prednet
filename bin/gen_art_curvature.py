70915b3572310db64d56177745810b29b3128611
AgeYY <zeyuanye@hotmail.com>
2022-03-29 18:26:54 -0500

    correct curvature, but small difference

--- bin/gen_art_curvature.py
+++ bin/gen_art_curvature.py
@@ -6,10 +6,15 @@ from scipy.optimize import minimize as sci_minimize
 from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 import numpy as np
+import os
 
+from predusion.immaker import Batch_gen
 from predusion.video_straight_reader import VS_reader
 from predusion.immaker import Batch_gen
 from predusion.curvature import Curvature
+from predusion.agent import Agent
+
+from kitti_settings import *
 
 class Artf_video():
     '''
@@ -66,18 +71,15 @@ class Artf_video():
         cos_theta = np.cos(theta_mat)
         cos_dot = 0
         for i in range(theta_mat.shape[0]-1):
-            #cos_dot += np.dot( cos_theta[i], cos_theta[i + 1] )
-            cos_dot += (np.dot( cos_theta[i], cos_theta[i + 1] ) - np.cos(tg_curvature))**2
-        #cos_dot = cos_dot / (theta_mat.shape[0]-2)
-        #loss1 = (cos_dot - np.cos(tg_curvature))**2
-        loss1 = cos_dot / (theta_mat.shape[0]-2)
+            cos_dot += np.dot( cos_theta[i], cos_theta[i + 1] )
+        cos_dot = cos_dot / (theta_mat.shape[0]-2)
+        loss1 = (cos_dot - np.cos(tg_curvature))**2
 
         loss2 = 0
         for i in range(theta_mat.shape[0]):
             loss2 += (np.linalg.norm(cos_theta[i]) - 1)**2
         loss2 = loss2 / theta_mat.shape[0]
 
-        print(loss1 + self.alpha * loss2)
         return loss1 + self.alpha * loss2
 
     def minimize(self, tg_curvature, options=None):
@@ -109,9 +111,9 @@ class Artf_video():
         '''
         video_pca = np.zeros( (self.n_frame, self.n_component) )
         #video_pca[0] = self.x0_pca
-        video_pca[0] = np.random.uniform(low=0, high=1, size=(self.x0_pca.shape))
+        video_pca[0] = np.random.uniform(low=0, high=1, size=(self.x0_pca.shape)) * 10 + 10
         for i in range(theta_mat.shape[0]): # from the fist frame to the last one
-            video_pca[i + 1] = video_pca[i] + self.vec_norm_pca[i] * theta_mat[i] # the shape is 
+            video_pca[i + 1] = video_pca[i] + self.vec_norm_pca[i] * np.cos(theta_mat[i]) # the shape is 
         # convert theta to the artificial video
         video = np.zeros(( self.n_frame, *self.imshape ))
         for i, frame in enumerate(video_pca):
@@ -120,7 +122,7 @@ class Artf_video():
 
 imshape = (128, 160)
 video_type = 'natural'
-video_cate = '01'
+video_cate = '05'
 scale = '1x'
 n_component_video = 5 # the curvature is calculated after dimension reduction to n_component_video
 
@@ -151,6 +153,88 @@ print(tg_curvature)
 print(result.fun)
 
 ########## Show the video
-for im in video:
-    plt.imshow(im)
-    plt.show()
+#for im in video:
+#    plt.imshow(im)
+#    plt.show()
+
+##### load the prednet
+json_file = os.path.join(WEIGHTS_DIR, 'prednet_kitti_model.json')
+weights_file = os.path.join(WEIGHTS_DIR, 'tensorflow_weights/prednet_kitti_weights.hdf5')
+n_component = 5
+
+video_ppd = Batch_gen.process_grey_video(artf_video[None, ...], imshape=imshape) # process the video
+
+batch_size = video_ppd.shape[0]
+
+sub = Agent()
+sub.read_from_json(json_file, weights_file)
+
+
+output = sub.output_multiple(video_ppd, output_mode=output_mode, batch_size=batch_size) # if output is not prediction, the output shape would be (batch_size, number of images in a seq, a 3d tensor represent neural activation)
+
+########## curverature origin
+cv = Curvature()
+
+ct_mean = {}
+ct_mean_pca = {}
+
+for key in output:
+    output[key] = output[key].reshape(output[key].shape[0], output[key].shape[1], -1) # flatten (n_video, n_frames, n_neurons)
+    cv.load_data(output[key])
+
+    ct_mean[key] = cv.curvature_traj(cutoff=cutoff, n_component=None) # ignore the first two video frames due to the pool prediction of the prednet
+    ct_mean_pca[key] = cv.curvature_traj(cutoff=cutoff, n_component=n_component) # ignore the first two video frames due to the pool prediction of the prednet
+
+video_flat = video.reshape((video.shape[0], video.shape[1], -1))
+cv.load_data(video_flat)
+ct_mean_video = cv.curvature_traj(cutoff=cutoff)
+
+ct_mean_video_pca = cv.curvature_traj(cutoff=cutoff, n_component=n_component)
+
+########## Change of curvature
+ct_mean_change = {}
+ct_mean_change_pca = {}
+
+ct_mean_change_median = {}
+ct_mean_change_pca_median = {}
+
+ct_mean_change_ci = {}
+ct_mean_change_pca_ci = {}
+
+for key in ct_mean:
+    ct_mean_change[key] = ct_mean[key] - ct_mean_video
+    ct_mean_change_median[key] = np.median(ct_mean_change[key])
+    ct_mean_change_ci[key] = np.array( confidence_interval(ct_mean_change[key], ci_bound=ci_bound, measure=np.median, n_resamples=ci_resample) )
+
+    ct_mean_change_pca[key] = ct_mean_pca[key] - ct_mean_video_pca
+    ct_mean_change_pca_median[key] = np.median(ct_mean_change_pca[key])
+    ct_mean_change_pca_ci[key] = np.array( confidence_interval(ct_mean_change_pca[key], ci_bound=ci_bound, measure=np.median, n_resamples=ci_resample) )
+
+########## plot out the median
+def plot_curv_layer(ct_mean_change_median, ct_mean_change_ci):
+    for key in ct_mean_change_median:
+        x, y = [int(key[1])], [ct_mean_change_median[key]]
+        yerr = (ct_mean_change_ci[key] - y)[..., None]
+        yerr = np.abs(yerr)
+        if 'E' in key:
+            plt.scatter(x, y, c='green')
+            plt.errorbar(x, y, yerr=yerr, c='green')
+        if 'R' in key:
+            plt.scatter(x, y, c='red')
+            plt.errorbar(x, y, yerr=yerr, c='red')
+
+plt.figure()
+plot_curv_layer(ct_mean_change_median, ct_mean_change_ci)
+plt.axhline(y = 0, linestyle = '--', linewidth = 1, color = 'black')
+plt.legend()
+plt.xlabel('Module ID')
+plt.ylabel('Change of curvature \n (curvature of neurons - curvature of videos)')
+plt.show()
+
+plt.figure()
+plot_curv_layer(ct_mean_change_pca_median, ct_mean_change_pca_ci)
+plt.axhline(y = 0, linestyle = '--', linewidth = 1, color = 'black')
+plt.legend()
+plt.xlabel('Module ID')
+plt.ylabel('Change of curvature \n (curvature of neurons - curvature of videos)')
+plt.show()
