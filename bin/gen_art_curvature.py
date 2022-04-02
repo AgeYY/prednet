ebc47c7ac44f5bf8041274240725360089b031c3
AgeYY <42469123+AgeYY@users.noreply.github.com>
2022-04-02 16:59:34 -0500

    new artificial curvature based directly on angle

--- bin/gen_art_curvature.py
+++ bin/gen_art_curvature.py
@@ -16,6 +16,110 @@ from predusion.agent import Agent
 
 from kitti_settings import *
 
+#class Artf_video():
+#    '''
+#    generate artificial videos with mean curvature equal to the target curvature. The norm of the difference between two adjacent video frames are the same. This can also avoid two frames non-identical, so that curvature can be normally computed
+#    '''
+#    def __init__(self, n_component=5, alpha=1):
+#        self.cv = Curvature()
+#        self.pca = PCA(n_components=n_component)
+#        self.n_component = n_component
+#        self.alpha=alpha # the hyperparameter in contronling the norm = 1 in loss function
+#        self.nat_video = None
+#
+#    def set_n_component(self, n_component):
+#        self.n_component=n_component
+#        self.pca = PCA(n_components=n_component)
+#
+#        if not (self.nat_video is None):
+#            self.load_natural_video(self.nat_video)
+#
+#    def load_natural_video(self, nat_video):
+#        '''
+#        input:
+#          nat_video ([n_frame, imshape[0], imshape[1]])
+#        stored data:
+#          self.norm_pca (array, float, [n_frame - 1]): norm of x_(i+1)_pca - x_i_pca
+#        '''
+#        # store the video
+#        self.nat_video = nat_video
+#        # store parameters: n_frame, imshape
+#        self.n_frame = video.shape[0]
+#        self.imshape = (video.shape[1:])
+#        # do the pca
+#        nat_video_pca = self.pca.fit_transform(nat_video.reshape(self.n_frame, -1))
+#        # store x0
+#        self.x0_pca = nat_video_pca[0]
+#        # store the norm
+#        self.vec_norm_pca = np.linalg.norm(
+#            np.diff( nat_video_pca, axis=0), \
+#            axis=1
+#        )
+#
+#    def curv_natural_video():
+#        pass
+#
+#    def loss_curv(self, theta, tg_curvature):
+#        '''
+#        loss function: (sum{ cos(\theta_i) \dot cos(\theta_i+1) }/(n - 2) - cos(tg_curvature))^2 + alpha * sum{ norm{cos(\theta_i)} - 1}^2 / (n-1)
+#        input:
+#          theta (array, float, [(self.n_frame - 1) * self.n_component])
+#        output:
+#          loss (float)
+#        '''
+#        theta_mat = theta.reshape(-1, self.n_component) # [(n_frame - 1), n_compoent]
+#        cos_theta = np.cos(theta_mat)
+#        cos_dot = 0
+#        for i in range(theta_mat.shape[0]-1):
+#            cos_dot += np.dot( cos_theta[i], cos_theta[i + 1] )
+#        cos_dot = cos_dot / (theta_mat.shape[0]-2)
+#        loss1 = (cos_dot - np.cos(tg_curvature))**2
+#
+#        loss2 = 0
+#        for i in range(theta_mat.shape[0]):
+#            loss2 += (np.linalg.norm(cos_theta[i]) - 1)**2
+#        loss2 = loss2 / theta_mat.shape[0]
+#
+#        return loss1 + self.alpha * loss2
+#
+#    def minimize(self, tg_curvature, options=None):
+#        '''
+#        minimize the loss function given video_init and target curvature
+#        input:
+#          tg_curvature (float): the target curvature
+#          random_seed (int)
+#        output:
+#        '''
+#        # initialize theta, as an flatten array. shape? Let's say shape of natural video is (n_frame - 2) * reduced dimension (after PCA)
+#        theta = np.random.uniform(low=0, high=2 * np.pi, size=(self.n_frame - 1) * self.n_component)
+#        # feed theta into the minimize function
+#        result = sci_minimize(self.loss_curv, theta, args=(tg_curvature), options=options)
+#        # get the result theta from result
+#        theta_mat = result.x.reshape((n_frame-1, self.n_component))
+#        # reconstruct the reduced video
+#        art_video = self.recons_video(theta_mat)
+#        # convert the dimension
+#        return art_video, result
+#
+#    def recons_video(self, theta_mat):
+#        '''
+#        reconstruct the video based on theta
+#        input:
+#          theta_mat (array, float, [self.n_frame - 1, self.n_component])
+#        output:
+#          art_video (array, float, same shape as the natural image)
+#        '''
+#        video_pca = np.zeros( (self.n_frame, self.n_component) )
+#        #video_pca[0] = self.x0_pca
+#        video_pca[0] = np.random.uniform(low=0, high=1, size=(self.x0_pca.shape))
+#        for i in range(theta_mat.shape[0]): # from the fist frame to the last one
+#            video_pca[i + 1] = video_pca[i] + self.vec_norm_pca[i] * np.cos(theta_mat[i]) # the shape is 
+#        # convert theta to the artificial video
+#        video = np.zeros(( self.n_frame, *self.imshape ))
+#        for i, frame in enumerate(video_pca):
+#            video[i] = self.pca.inverse_transform( frame ).reshape(self.imshape)
+#        return video
+
 class Artf_video():
     '''
     generate artificial videos with mean curvature equal to the target curvature. The norm of the difference between two adjacent video frames are the same. This can also avoid two frames non-identical, so that curvature can be normally computed
@@ -59,28 +163,23 @@ class Artf_video():
     def curv_natural_video():
         pass
 
-    def loss_curv(self, theta, tg_curvature):
+    def loss_curv(self, tildew, tg_curvature):
         '''
         loss function: (sum{ cos(\theta_i) \dot cos(\theta_i+1) }/(n - 2) - cos(tg_curvature))^2 + alpha * sum{ norm{cos(\theta_i)} - 1}^2 / (n-1)
         input:
-          theta (array, float, [(self.n_frame - 1) * self.n_component])
+          tildew (array, float, [(self.n_frame - 1) * self.n_component])
         output:
           loss (float)
         '''
-        theta_mat = theta.reshape(-1, self.n_component) # [(n_frame - 1), n_compoent]
-        cos_theta = np.cos(theta_mat)
-        cos_dot = 0
-        for i in range(theta_mat.shape[0]-1):
-            cos_dot += np.dot( cos_theta[i], cos_theta[i + 1] )
-        cos_dot = cos_dot / (theta_mat.shape[0]-2)
-        loss1 = (cos_dot - np.cos(tg_curvature))**2
+        tildew_mat = tildew.reshape(-1, self.n_component) # [(n_frame - 1), n_compoent]
+        ang = 0
+        for i in range(tildew_mat.shape[0]-1):
+            cos_dot = np.dot( tildew_mat[i], tildew_mat[i + 1] ) / np.linalg.norm(tildew_mat[i]) / np.linalg.norm(tildew_mat[i + 1])
+            ang += np.arccos(cos_dot)
+        ang = ang / (tildew_mat.shape[0]-1)
+        loss1 = (ang - tg_curvature)**2
 
-        loss2 = 0
-        for i in range(theta_mat.shape[0]):
-            loss2 += (np.linalg.norm(cos_theta[i]) - 1)**2
-        loss2 = loss2 / theta_mat.shape[0]
-
-        return loss1 + self.alpha * loss2
+        return loss1
 
     def minimize(self, tg_curvature, options=None):
         '''
@@ -91,17 +190,17 @@ class Artf_video():
         output:
         '''
         # initialize theta, as an flatten array. shape? Let's say shape of natural video is (n_frame - 2) * reduced dimension (after PCA)
-        theta = np.random.uniform(low=0, high=2 * np.pi, size=(self.n_frame - 1) * self.n_component)
+        tildew = np.random.uniform(low=0, high=1, size=(self.n_frame - 1) * self.n_component)
         # feed theta into the minimize function
-        result = sci_minimize(self.loss_curv, theta, args=(tg_curvature), options=options)
+        result = sci_minimize(self.loss_curv, tildew, args=(tg_curvature), options=options)
         # get the result theta from result
-        theta_mat = result.x.reshape((n_frame-1, self.n_component))
+        tildew_mat = result.x.reshape((self.n_frame-1, self.n_component))
         # reconstruct the reduced video
-        art_video = self.recons_video(theta_mat)
+        art_video = self.recons_video(tildew_mat)
         # convert the dimension
         return art_video, result
 
-    def recons_video(self, theta_mat):
+    def recons_video(self, tildew_mat):
         '''
         reconstruct the video based on theta
         input:
@@ -111,9 +210,9 @@ class Artf_video():
         '''
         video_pca = np.zeros( (self.n_frame, self.n_component) )
         #video_pca[0] = self.x0_pca
-        video_pca[0] = np.random.uniform(low=0, high=1, size=(self.x0_pca.shape)) * 10 + 10
-        for i in range(theta_mat.shape[0]): # from the fist frame to the last one
-            video_pca[i + 1] = video_pca[i] + self.vec_norm_pca[i] * np.cos(theta_mat[i]) # the shape is 
+        video_pca[0] = np.random.uniform(low=0, high=1, size=(self.x0_pca.shape))
+        for i in range(tildew_mat.shape[0]): # from the fist frame to the last one
+            video_pca[i + 1] = video_pca[i] + self.vec_norm_pca[i] * tildew_mat[i] / np.linalg.norm(tildew_mat[i]) # the shape is 
         # convert theta to the artificial video
         video = np.zeros(( self.n_frame, *self.imshape ))
         for i, frame in enumerate(video_pca):
@@ -151,7 +250,7 @@ artf_gen.load_natural_video(nat_video)
 artf_video, result = artf_gen.minimize(tg_curvature)
 
 cv.load_data(artf_video.reshape((1, nat_video.shape[0], -1)))
-artf_curv = cv.curvature_traj(n_component=n_component_video)
+artf_curv = cv.curvature_traj(n_component=5)
 print(artf_curv)
 print(tg_curvature)
 print(result.fun)
