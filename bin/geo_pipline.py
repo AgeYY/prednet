7864b92fb1f9058ddbc655751e45b852e32c8d4d
AgeYY <42469123+AgeYY@users.noreply.github.com>
2022-05-24 09:54:54 -0500

    small bug in visualize_neural_traj_speed

--- bin/geo_pipline.py
+++ bin/geo_pipline.py
@@ -1,14 +1,14 @@
 import os
 
-#os.system('python ./bin/gen_moving_bar_dataset.py')
+os.system('python ./bin/gen_moving_bar_dataset.py')
 
 #out_data_head_list = ['moving_bar_bg_color', 'moving_bar_obj_color']
 #out_data_head_list = ['moving_arc']
 #out_data_head_list = ['moving_text']
 #out_data_head_list = ['moving_bar_y']
-#out_data_head_list = ['moving_bar_wustl']
-out_data_head_list = ['moving_bar_on_video'] # make sure nt equal to the number of frames
+out_data_head_list = ['moving_bar_wustl']
+#out_data_head_list = ['moving_bar_on_video'] # make sure nt equal to the number of frames
 for out_data_head in out_data_head_list:
-    #os.system('python ./bin/gen_neural_res_moving_bar.py --data_head ' + out_data_head)
-    #os.system('python ./bin/moving_bar_geo.py --data_head ' + out_data_head)
+    os.system('python ./bin/gen_neural_res_moving_bar.py --data_head ' + out_data_head)
+    os.system('python ./bin/moving_bar_geo.py --data_head ' + out_data_head)
     os.system('python ./bin/visualize_neural_traj_speed.py --data_head ' + out_data_head)
