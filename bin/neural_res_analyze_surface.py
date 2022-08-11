1850ec61d229864401bc74e4ca1cf94813b38d4c
AgeYY <42469123+AgeYY@users.noreply.github.com>
2022-08-11 17:49:19 -0500

    angle prednet finished

--- bin/neural_res_analyze_surface.py
+++ bin/neural_res_analyze_surface.py
@@ -18,20 +18,20 @@ neural_data_path = 'neural_' + out_data_head + '_R_prednet' + '.hkl'
 label_path = 'label_' + out_data_head + '_R_prednet' + '.hkl'
 label_name_path = 'label_name_' + out_data_head + '_R_prednet' + '.hkl'
 
-mesh_size = 100
-label_id = (0, 1) # only fit manifold about these information variables.
+mesh_size = 20
+label_id = (0, 2) # only fit manifold about these information variables.
 train_ratio = 0.6
 test_ratio = 0.2
 explained_var_thre = 0.90
 explained_var_thre_pca_all_data = 0.90
 # drifting grating configurations
-lt0_mesh = np.linspace(0, 0.15, mesh_size)
-lt1_mesh = np.linspace(0, 90, mesh_size)
+lt0_mesh = np.linspace(0.03, 0.12, mesh_size)
+lt1_mesh = np.linspace(20, 70, mesh_size)
 lt2_mesh = np.linspace(0, 5, 30)
 lt_mesh = [lt0_mesh, lt1_mesh, lt2_mesh]
-kernel_width = [0.0001, 60, 0.1]
+kernel_width = [0.0001, 20, 0.1]
 
-## drifting grating configurations
+## random dot
 #lt0_mesh = np.linspace(0, 8, mesh_size) # the range should be larger than data
 #lt1_mesh = np.linspace(0, 6, mesh_size)
 #lt_mesh = [lt0_mesh, lt1_mesh]
@@ -61,7 +61,7 @@ geoa = manifold.Layer_manifold()
 # visualize testing
 geoa.load_data(feamap_train, label_train)
 # fit the manifold
-geoa.fit_info_manifold_all(lt_mesh, label_id, kernel_width=kernel_width)
+geoa.fit_info_manifold_grid_all(lt_mesh, label_id, kernel_width=kernel_width)
 # visualize infomation manifold
 n_layer, layer_order = layer_order_helper()
 for layer_name in layer_order:
@@ -79,22 +79,25 @@ for layer_name in layer_order:
 
 ############################ Fix the hyperparameter and repeat on different training testing sets on different random seeds
 train_ratio, test_ratio = 0.7, 0.3
-n_bootstrap = 20
-dim, score = {}, {}
+n_bootstrap = 2
+dim, score, angle = {}, {}, {}
 for i in range(n_bootstrap):
     (feamap_train, label_train), (feamap_test, label_test), (feamap_validate, label_validate) = train_test_validate_split(dataset, train_ratio, test_ratio, random_seed=None)
     geoa.load_data(feamap_train, label_train)
-    geoa.fit_info_manifold_all(lt_mesh, label_id, kernel_width=kernel_width)
+    geoa.fit_info_manifold_grid_all(lt_mesh, label_id, kernel_width=kernel_width)
     score_boots = geoa.manifold_decoder_score_all(feamap_test, label_test, label_id)
     dim_boots = geoa.dim_all(explained_var_thre, label_id)
+    angle_boots = geoa.angle_tangent_vec_all(label_id, label_id)
 
     for key in dim_boots:
         try:
             dim[key].append(dim_boots[key])
             score[key].append(score_boots[key])
+            angle[key] = np.append(angle[key], angle_boots[key])
         except KeyError:
             dim[key] = [dim_boots[key]]
             score[key] = [score_boots[key]]
+            angle[key] = angle_boots[key]
 
 ### visualize the dimensionality of the manifold
 n_layer, layer_order = layer_order_helper()
@@ -102,8 +105,15 @@ fig, ax = plt.subplots(figsize=(4, 4))
 ax = ploter.plot_layer_error_bar_helper(score, n_layer, layer_order, ax)
 ax.axhline(1, color='k', linestyle='--')
 ax.set_ylim(bottom=None, top=1)
+ax.set_title('score')
 plt.show()
 fig, ax = plt.subplots(figsize=(4, 4))
 ax = ploter.plot_layer_error_bar_helper(dim, n_layer, layer_order, ax)
 ax.axhline(0, color='k', linestyle='--')
+ax.set_title('dim')
+plt.show()
+fig, ax = plt.subplots(figsize=(4, 4))
+ax = ploter.plot_layer_error_bar_helper(angle, n_layer, layer_order, ax)
+ax.axhline(0, color='k', linestyle='--')
+ax.set_title('angle')
 plt.show()
