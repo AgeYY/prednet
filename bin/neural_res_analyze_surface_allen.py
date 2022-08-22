1824717b286229fcb35843b289bdc25b885e5081
AgeYY <42469123+AgeYY@users.noreply.github.com>
2022-08-22 17:50:18 -0500

    finished output allen neural data, nan encoutered in analyze

---
+++ bin/neural_res_analyze_surface_allen.py
@@ -0,0 +1,121 @@
+# analyze the manifold of two variables (surfaces)
+import os
+import numpy as np
+import hickle as hkl
+import matplotlib.pyplot as plt
+from sklearn.model_selection import GridSearchCV
+
+import predusion.manifold as manifold
+import predusion.ploter as ploter
+from predusion.ploter import Ploter_dim_reduction
+from predusion.static_dataset import Layer_Dataset, train_test_validate_split
+from kitti_settings import *
+
+out_data_head = 'static_grating'
+#out_data_head = 'dot_stim'
+output_mode = ['VISp']
+#neural_data_path = 'neural_' + out_data_head + '_R_prednet' + '.hkl'
+#label_path = 'label_' + out_data_head + '_R_prednet' + '.hkl'
+#label_name_path = 'label_name_' + out_data_head + '_R_prednet' + '.hkl'
+neural_data_path = 'neural_' + out_data_head + '_allen_VISp' + '.hkl'
+label_path = 'label_' + out_data_head + '_allen_VISp' + '.hkl'
+label_name_path = 'label_name_' + out_data_head + '_allen_VISp' + '.hkl'
+
+mesh_size = 20
+label_id = (0,) # only fit manifold about these information variables.
+train_ratio = 0.6
+test_ratio = 0.2
+explained_var_thre = 0.90
+explained_var_thre_pca_all_data = 0.90
+# drifting grating configurations
+lt0_mesh = np.linspace(0.03, 0.12, mesh_size)
+lt1_mesh = np.linspace(20, 70, mesh_size)
+lt2_mesh = np.linspace(1, 4, 30)
+lt_mesh = [lt0_mesh, lt1_mesh, lt2_mesh]
+kernel_width = [0.0001, 20, 0.1]
+
+## random dot
+#lt0_mesh = np.linspace(0, 8, mesh_size) # the range should be larger than data
+#lt1_mesh = np.linspace(0, 6, mesh_size)
+#lt_mesh = [lt0_mesh, lt1_mesh]
+#kernel_width = [0.5, 1]
+
+feamap_path = os.path.join(DATA_DIR, neural_data_path)
+label_path = os.path.join(DATA_DIR, label_path)
+label_name_path = os.path.join(DATA_DIR, label_name_path)
+
+def layer_order_helper():
+    n_layer = 1
+    layer_order = ['VISp']
+    return n_layer, layer_order
+
+dataset = Layer_Dataset(feamap_path, label_path, label_name_path, explained_var_thre=explained_var_thre_pca_all_data)
+
+geoa = manifold.Layer_manifold()
+
+############################### Use grid search in a single train/validate/test split to find optimal kernel_width
+#kernel_width_list = [ [0.1], [0.01], [0.001], [0.0001], [0.00001], [0.000001], [0.0000001]]
+###kernel_width_list = [ [5], [10], [30], [50], [70], [90], [110] ]
+(feamap_train, label_train), (feamap_test, label_test), (feamap_validate, label_validate) = train_test_validate_split(dataset, train_ratio, test_ratio, random_seed=42)
+#score = geoa.search_kernel_width(lt_mesh, feamap_train, label_train, feamap_validate, label_validate, label_id, kernel_width_list)
+#[print(key,':',value) for key, value in score.items()]
+
+# visualize testing
+geoa.load_data(feamap_train, label_train)
+# fit the manifold
+geoa.fit_info_manifold_grid_all(lt_mesh, label_id, kernel_width=kernel_width)
+# visualize infomation manifold
+n_layer, layer_order = layer_order_helper()
+for layer_name in layer_order:
+    info_manifold = geoa.ana_group[layer_name][label_id].info_manifold.copy()
+    label_mesh = geoa.ana_group[layer_name][label_id].label_mesh.copy()
+
+    plt_dr = Ploter_dim_reduction(method='pca', n_components=2)
+    for i, lb_id in enumerate(label_id):
+        vmin, vmax = label_mesh[:, i].min(), label_mesh[:, i].max()
+        fig, ax =  plt_dr.plot_dimension_reduction(info_manifold, colorinfo=label_mesh[:, i], mode='2D', fit=True, vmin=vmin, vmax=vmax)
+        #fig, ax =  plt_dr.plot_dimension_reduction(feamap_validate[layer_name], colorinfo=label_validate[:, lb_id], mode='2D', fig=fig, ax=ax, marker='+', vmin=vmin, vmax=vmax) # test
+        fig, ax =  plt_dr.plot_dimension_reduction(feamap_test[layer_name], colorinfo=label_test[:, lb_id], mode='2D', fig=fig, ax=ax, marker='+', vmin=vmin, vmax=vmax) # test
+        plt.show()
+############################## visualize manifold
+
+############################ Fix the hyperparameter and repeat on different training testing sets on different random seeds
+train_ratio, test_ratio = 0.7, 0.3
+n_bootstrap = 20
+dim, score, angle = {}, {}, {}
+for i in range(n_bootstrap):
+    (feamap_train, label_train), (feamap_test, label_test), (feamap_validate, label_validate) = train_test_validate_split(dataset, train_ratio, test_ratio, random_seed=None)
+    geoa.load_data(feamap_train, label_train)
+    geoa.fit_info_manifold_grid_all(lt_mesh, label_id, kernel_width=kernel_width)
+    score_boots = geoa.manifold_decoder_score_all(feamap_test, label_test, label_id)
+    dim_boots = geoa.dim_all(explained_var_thre, label_id)
+    angle_boots = geoa.angle_tangent_vec_all(label_id, label_id)
+
+    for key in dim_boots:
+        try:
+            dim[key].append(dim_boots[key])
+            score[key].append(score_boots[key])
+            angle[key] = np.append(angle[key], angle_boots[key])
+        except KeyError:
+            dim[key] = [dim_boots[key]]
+            score[key] = [score_boots[key]]
+            angle[key] = angle_boots[key]
+
+### visualize the dimensionality of the manifold
+n_layer, layer_order = layer_order_helper()
+fig, ax = plt.subplots(figsize=(4, 4))
+ax = ploter.plot_layer_error_bar_helper(score, n_layer, layer_order, ax)
+ax.axhline(1, color='k', linestyle='--')
+ax.set_ylim(bottom=None, top=1)
+ax.set_title('score')
+plt.show()
+fig, ax = plt.subplots(figsize=(4, 4))
+ax = ploter.plot_layer_error_bar_helper(dim, n_layer, layer_order, ax)
+ax.axhline(0, color='k', linestyle='--')
+ax.set_title('dim')
+plt.show()
+fig, ax = plt.subplots(figsize=(4, 4))
+ax = ploter.plot_layer_error_bar_helper(angle, n_layer, layer_order, ax)
+ax.axhline(0, color='k', linestyle='--')
+ax.set_title('angle')
+plt.show()
--- test/test_brain_obs.py
+++ test/test_brain_obs.py
@@ -9,106 +9,126 @@ import hickle as hkl
 from allensdk.core.brain_observatory_cache import BrainObservatoryCache
 from kitti_settings import *
 
-output_name = 'neural_moving_bar_R' + '.hkl'
+out_data_head = 'static_grating' # currently we don't calculate the label yet
 
-drive_path = '../data/allen-brain-observatory/visual-coding-2p'
-# drive_path = ''
-manifest_file = os.path.join(drive_path,'manifest.json')
+# load the BOC
+drive_path = './data/allen-brain-observatory/visual-coding-2p'
+manifest_file = os.path.join(drive_path, 'manifest.json')
 
 boc = BrainObservatoryCache(manifest_file=manifest_file)
 
 # download one experiment
-exp_id = 511510699
-exps = boc.get_ophys_experiments(experiment_container_ids=[exp_id], stimuli=['drifting_gratings'])
+exp_id = 511510699 # region VISp
+exps = boc.get_ophys_experiments(experiment_container_ids=[exp_id], stimuli=['static_gratings'])
 print("Experiments for experiment_container_id %d: %d\n" % (exp_id, len(exps)))
 pprint.pprint(exps)
 
-session_id = exps[0]['id']
+session_id = exps[0]['id'] # only one session has static_gratings, download it directly
 data_set = boc.get_ophys_experiment_data(session_id)
 
 ts, dff = data_set.get_dff_traces()
-#print(dff.shape)
+print(dff.shape)
 
-fig = plt.figure(figsize=(10,8))
-#for i in range(50):
+#fig = plt.figure(figsize=(10,8))
+#for i in range(50): # show the first 50 DFF traces
 #    plt.plot(dff[i]+(i*2), color='gray')
 #plt.show()
 
-stim_table = data_set.get_stimulus_table('drifting_gratings')
+stim_table = data_set.get_stimulus_table('static_gratings')
 
 #print(stim_table.head())
+# get all dff for each stimulus
 
-cell_response = {'orientation': [], 'temporal_frequency': [], 'response': []}
+label_dic = {'orientation': [], 'spatial_frequency': [], 'phase': []}
+feamap = []
 for i in range(len(stim_table)):
-    cell_response['orientation'].append(stim_table['orientation'][i])
-    cell_response['temporal_frequency'].append(stim_table['temporal_frequency'][i])
-    res_cell = dff[:, stim_table['start'][i]: stim_table['end'][i]]
-    cell_response['response'].append(res_cell)
-cell_response = pd.DataFrame(cell_response)
+    for key in label_dic:
+        label_dic[key].append(stim_table[key][i])
+    res_cell = np.mean( dff[:, stim_table['start'][i]: stim_table['end'][i]], axis=1 ) # average response through the whole trial period
+    feamap.append(res_cell)
+
+label_name = list(label_dic.keys())
+label = [label_dic[key] for key in label_dic]
+
+label = np.array(label).T
+feamap = {'VISp': np.array(feamap)}
+
+neural_data_path = 'neural_' + out_data_head + '_allen_VISp' + '.hkl'
+label_path = 'label_' + out_data_head + '_allen_VISp' + '.hkl'
+label_name_path = 'label_name_' + out_data_head + '_allen_VISp' + '.hkl'
+
+feamap_path = os.path.join(DATA_DIR, neural_data_path)
+label_path = os.path.join(DATA_DIR, label_path)
+label_name_path = os.path.join(DATA_DIR, label_name_path)
+
+hkl.dump(feamap, feamap_path)
+hkl.dump(label, label_path)
+hkl.dump(label_name, label_name_path)
+
 #print(cell_response.head())
-
-unique_orientation = np.sort(cell_response['orientation'].dropna().unique())
-unique_temporal_frequency = np.sort(cell_response['temporal_frequency'].dropna().unique())
-
-def min_time_points(crs):
-    '''
-    crs (series, [n_trails]): each element has shape [n_cell, n_time], but the number of time points may not be the same. This function cut time points to make sure the number of time points in each trial are the same
-    '''
-    cell_response_same_tf = copy.deepcopy(crs)
-    min_n_time = 9999999
-    for cell_response_trial in cell_response_same_tf:
-        min_n_time = min(len(cell_response_trial[0]), min_n_time)
-
-    for i in range( len(cell_response_same_tf) ):
-        cell_response_same_tf.iloc[i] = np.array(cell_response_same_tf.iloc[i])[:, :min_n_time]
-
-    return cell_response_same_tf
-
-
-# take average of orientation
-cell_response_tf = {tf: [] for tf in unique_temporal_frequency}
-for tf in cell_response_tf:
-    cell_response_same_tf = cell_response[ cell_response['temporal_frequency']==tf ]['response']
-    cell_response_same_tf = min_time_points(cell_response_same_tf)
-    cell_response_tf[tf] = cell_response_same_tf.mean()
-
-#for cell in cell_response_tf[15.0]:
-#    plt.plot(range(len(cell)), cell, color='grey')
-#plt.show()
-
-# the expected shape should be [n_trial, n_time, n_cells]
-n_time = cell_response_tf[15.0].shape[1]
-n_cell = cell_response_tf[15.0].shape[0]
-n_tf = len(cell_response_tf)
-
-response = np.empty((n_tf, n_time, n_cell))
-for i, tf in enumerate(cell_response_tf):
-    response[i] = np.nan_to_num(cell_response_tf[tf].T, nan=0)
-res_dic  = {'R0': response}
-
-hkl.dump(res_dic, os.path.join(DATA_DIR, output_name))
-
-# in order to use the api of mvoing_bar_geo, we would like to create fake stimuli based on drifting grating
-
-from predusion.immaker import Moving_dot
-from predusion import im_processor as impor
-from PIL import Image, ImageDraw
-import hickle as hkl
-import numpy as np
-
-width = 200
-step = n_time
-speed_list = range(1, n_tf) # step = 12, speed_list_max = 12 make sure the squre doesn't fall out of the image
-
-out_data_head = 'mice_tf'
-category = out_data_head
-
-dg = Moving_dot()
-speed_list = np.linspace(0.02, 0.08, n_tf)
-sf = 0.02
-
-dg.set_stim_obj(obj_name='GratingStim', sf=sf)
-
-dg.create_video_batch(speed_list=speed_list, n_frame=step, category=out_data_head)
-categories = [out_data_head]
-impor.process_data(categories, out_data_head=out_data_head) #!!!!!!!!! WARNING the label may not be in the correct sequence as response
+#
+#unique_orientation = np.sort(cell_response['orientation'].dropna().unique())
+#unique_temporal_frequency = np.sort(cell_response['temporal_frequency'].dropna().unique())
+#
+#def min_time_points(crs):
+#    '''
+#    crs (series, [n_trails]): each element has shape [n_cell, n_time], but the number of time points may not be the same. This function cut time points to make sure the number of time points in each trial are the same
+#    '''
+#    cell_response_same_tf = copy.deepcopy(crs)
+#    min_n_time = 9999999
+#    for cell_response_trial in cell_response_same_tf:
+#        min_n_time = min(len(cell_response_trial[0]), min_n_time)
+#
+#    for i in range( len(cell_response_same_tf) ):
+#        cell_response_same_tf.iloc[i] = np.array(cell_response_same_tf.iloc[i])[:, :min_n_time]
+#
+#    return cell_response_same_tf
+#
+#
+## take average of orientation
+#cell_response_tf = {tf: [] for tf in unique_temporal_frequency}
+#for tf in cell_response_tf:
+#    cell_response_same_tf = cell_response[ cell_response['temporal_frequency']==tf ]['response']
+#    cell_response_same_tf = min_time_points(cell_response_same_tf)
+#    cell_response_tf[tf] = cell_response_same_tf.mean()
+#
+##for cell in cell_response_tf[15.0]:
+##    plt.plot(range(len(cell)), cell, color='grey')
+##plt.show()
+#
+## the expected shape should be [n_trial, n_time, n_cells]
+#n_time = cell_response_tf[15.0].shape[1]
+#n_cell = cell_response_tf[15.0].shape[0]
+#n_tf = len(cell_response_tf)
+#
+#response = np.empty((n_tf, n_time, n_cell))
+#for i, tf in enumerate(cell_response_tf):
+#    response[i] = np.nan_to_num(cell_response_tf[tf].T, nan=0)
+#res_dic  = {'R0': response}
+#
+#hkl.dump(res_dic, os.path.join(DATA_DIR, output_name))
+#
+## in order to use the api of mvoing_bar_geo, we would like to create fake stimuli based on drifting grating
+#
+#from predusion.immaker import Moving_dot
+#from predusion import im_processor as impor
+#from PIL import Image, ImageDraw
+#import hickle as hkl
+#import numpy as np
+#
+#width = 200
+#step = n_time
+#speed_list = range(1, n_tf) # step = 12, speed_list_max = 12 make sure the squre doesn't fall out of the image
+#
+#out_data_head = 'mice_tf'
+#category = out_data_head
+#
+#dg = Moving_dot()
+#speed_list = np.linspace(0.02, 0.08, n_tf)
+#sf = 0.02
+#
+#dg.set_stim_obj(obj_name='GratingStim', sf=sf)
+#
+#dg.create_video_batch(speed_list=speed_list, n_frame=step, category=out_data_head)
+#categories = [out_data_head]
+#impor.process_data(categories, out_data_head=out_data_head) #!!!!!!!!! WARNING the label may not be in the correct sequence as response
